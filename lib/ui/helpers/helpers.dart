0ff6172348ed4a454f583f34f6d1c4156f852ab5
Caio Jesus <caio.nascimento@grupofasitec.com.br>
2021-08-02 15:18:08 -0300

    feat: add semantics label to CustomTextField widget

--- lib/ui/helpers/helpers.dart
+++ lib/ui/helpers/helpers.dart
@@ -1,1 +1,2 @@
 export 'app_theme.dart';
+export 'app_colors.dart';
--- lib/ui/widgets/custom_textfield_widget.dart
+++ lib/ui/widgets/custom_textfield_widget.dart
@@ -5,36 +5,44 @@ class CustomTextFieldWidget extends StatelessWidget {
   final String labelText;
   final TextEditingController controller;
   final TextInputType inputType;
+  final bool obscureText;
+  final String semanticsLabel;
 
   const CustomTextFieldWidget({
     Key? key,
     required this.labelText,
     required this.controller,
     required this.inputType,
+    required this.obscureText,
+    required this.semanticsLabel,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return TextFormField(
-      keyboardType: inputType,
-      controller: controller,
-      decoration: InputDecoration(
-        labelText: labelText,
-        labelStyle: Theme.of(context).textTheme.bodyText1,
-        border: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(10),
-        ),
-        enabledBorder: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(10),
-          borderSide: BorderSide(width: 2, color: accentColor),
-        ),
-        focusedBorder: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(10),
-          borderSide: BorderSide(width: 2, color: primaryColor),
-        ),
-        errorBorder: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(10),
-          borderSide: BorderSide(width: 2, color: Colors.redAccent),
+    return Semantics(
+      label: semanticsLabel,
+      child: TextFormField(
+        keyboardType: inputType,
+        controller: controller,
+        obscureText: obscureText,
+        decoration: InputDecoration(
+          labelText: labelText,
+          labelStyle: Theme.of(context).textTheme.bodyText1,
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(10),
+          ),
+          enabledBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(10),
+            borderSide: BorderSide(width: 2, color: accentColor),
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(10),
+            borderSide: BorderSide(width: 2, color: primaryColor),
+          ),
+          errorBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(10),
+            borderSide: BorderSide(width: 2, color: Colors.redAccent),
+          ),
         ),
       ),
     );
