cd6f63c1f2e38512b2918e1ec2b6aff713487775
Caio Jesus <caio.nascimento@grupofasitec.com.br>
2021-08-02 13:23:03 -0300

    feat: add app colors

---
+++ lib/ui/helpers/app_colors.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+const primaryColor = Color(0xff755FE2);
+const primaryColorDark = Color(0xff393D69);
+
+const backgroundColor = Color(0xff2A3C44);
+
+const orangeCardColor = Color(0xffFF565E);
+const greenCardColor = Color(0xff00A88B);
+
+const accentColor = Color(0xff224957);
