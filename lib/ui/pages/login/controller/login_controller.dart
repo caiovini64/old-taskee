ae7a2d29ede889f629fc86c03b7845fa9ed5485a
Caio Jesus <caio.nascimento@grupofasitec.com.br>
2021-08-02 15:57:19 -0300

    feat: add input validators

--- lib/ui/pages/login/controller/login_controller.dart
+++ lib/ui/pages/login/controller/login_controller.dart
@@ -10,4 +10,17 @@ class LoginController extends Cubit<LoginState> {
   void login(String email, String password) {
     emit(LoginLoading());
   }
+
+  String? validatePassword(String? value) {
+    if (value!.length < 6 && value.isEmpty)
+      return 'Password should contain more than 6 characters';
+    return null;
+  }
+
+  String? validateEmail(String? value) {
+    bool emailValid = RegExp(r'^.+@[a-zA-Z]+\.{1}[a-zA-Z]+(\.{0,1}[a-zA-Z]+)$')
+        .hasMatch(value!);
+    if (!emailValid) return 'Invalid email!';
+    return null;
+  }
 }
--- lib/ui/pages/login/login_page.dart
+++ lib/ui/pages/login/login_page.dart
@@ -8,6 +8,7 @@ import 'package:taskee/ui/widgets/widgets.dart';
 import 'controller/login_controller.dart';
 
 class LoginPage extends StatelessWidget {
+  final _formKey = GlobalKey<FormState>();
   final emailController = TextEditingController();
   final passwordController = TextEditingController();
 
@@ -19,87 +20,99 @@ class LoginPage extends StatelessWidget {
         return Scaffold(
           resizeToAvoidBottomInset: false,
           body: Container(
-            child: Column(
-              children: [
-                SizedBox(height: 100),
-                Expanded(
-                  child: SingleChildScrollView(
-                    scrollDirection: Axis.vertical,
-                    child: Container(
-                      height: MediaQuery.of(context).size.height,
-                      padding: EdgeInsets.all(20),
-                      color: backgroundColor,
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: [
-                          SizedBox(height: 35),
-                          Align(
-                            alignment: Alignment.topLeft,
-                            child: Text(
-                              'Wellcome!',
-                              style: Theme.of(context).textTheme.headline1,
+            child: Form(
+              key: _formKey,
+              child: Column(
+                children: [
+                  SizedBox(height: 100),
+                  Expanded(
+                    child: SingleChildScrollView(
+                      scrollDirection: Axis.vertical,
+                      child: Container(
+                        height: MediaQuery.of(context).size.height,
+                        padding: EdgeInsets.all(20),
+                        color: backgroundColor,
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            SizedBox(height: 35),
+                            Align(
+                              alignment: Alignment.topLeft,
+                              child: Text(
+                                'Wellcome!',
+                                style: Theme.of(context).textTheme.headline1,
+                              ),
                             ),
-                          ),
-                          SizedBox(height: 100),
-                          CustomTextFieldWidget(
-                            labelText: 'Login',
-                            semanticsLabel: 'Email text field',
-                            inputType: TextInputType.emailAddress,
-                            controller: emailController,
-                            obscureText: false,
-                          ),
-                          SizedBox(height: 35),
-                          CustomTextFieldWidget(
-                            labelText: 'Password',
-                            semanticsLabel: 'Password text field',
-                            inputType: TextInputType.visiblePassword,
-                            controller: passwordController,
-                            obscureText: true,
-                          ),
-                          SizedBox(height: 35),
-                          ElevatedButton(
-                            child: state is LoginLoading
-                                ? CircularProgressIndicator(
-                                    color: Colors.white,
-                                  )
-                                : Text('Login'),
-                            style: ElevatedButton.styleFrom(
-                              minimumSize: Size(400, 60),
-                              primary: primaryColor,
+                            SizedBox(height: 100),
+                            CustomTextFieldWidget(
+                              labelText: 'Login',
+                              semanticsLabel: 'Email text field',
+                              inputType: TextInputType.emailAddress,
+                              controller: emailController,
+                              obscureText: false,
+                              validator: (value) =>
+                                  controller.validateEmail(value),
+                            ),
+                            SizedBox(height: 35),
+                            CustomTextFieldWidget(
+                              labelText: 'Password',
+                              semanticsLabel: 'Password text field',
+                              inputType: TextInputType.visiblePassword,
+                              controller: passwordController,
+                              obscureText: true,
+                              validator: (value) =>
+                                  controller.validatePassword(value),
+                            ),
+                            SizedBox(height: 35),
+                            ElevatedButton(
+                              child: state is LoginLoading
+                                  ? CircularProgressIndicator(
+                                      color: Colors.white,
+                                    )
+                                  : Text('Login'),
+                              style: ElevatedButton.styleFrom(
+                                minimumSize: Size(400, 60),
+                                primary: primaryColor,
+                                shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(10)),
+                              ),
+                              onPressed: () {
+                                final validate =
+                                    _formKey.currentState!.validate();
+                                if (validate) {
+                                  final email = emailController.value.text;
+                                  final password =
+                                      passwordController.value.text;
+                                  controller.login(email, password);
+                                }
+                              },
+                            ),
+                            SizedBox(height: 35),
+                            ElevatedButton(
+                              child: Text('Create new account'),
+                              style: ElevatedButton.styleFrom(
+                                minimumSize: Size(400, 60),
+                                primary: primaryColorDark,
+                                onPrimary: primaryColor,
+                                shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(10)),
+                              ),
+                              onPressed: () {},
+                            ),
+                            SizedBox(height: 35),
+                            SignInButton(
+                              Buttons.Google,
                               shape: RoundedRectangleBorder(
                                   borderRadius: BorderRadius.circular(10)),
+                              onPressed: () {},
                             ),
-                            onPressed: () {
-                              final email = emailController.value.text;
-                              final password = passwordController.value.text;
-                              controller.login(email, password);
-                            },
-                          ),
-                          SizedBox(height: 35),
-                          ElevatedButton(
-                            child: Text('Create new account'),
-                            style: ElevatedButton.styleFrom(
-                              minimumSize: Size(400, 60),
-                              primary: primaryColorDark,
-                              onPrimary: primaryColor,
-                              shape: RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.circular(10)),
-                            ),
-                            onPressed: () {},
-                          ),
-                          SizedBox(height: 35),
-                          SignInButton(
-                            Buttons.Google,
-                            shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(10)),
-                            onPressed: () {},
-                          ),
-                        ],
+                          ],
+                        ),
                       ),
                     ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
           ),
         );
