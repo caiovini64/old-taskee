c3c801746ed0728a21baa774152703a674b1983b
Caio Jesus <caio.nascimento@grupofasitec.com.br>
2021-08-05 10:32:24 -0300

    feat: create TaskModel

--- lib/domain/entities/task_entity.dart
+++ lib/domain/entities/task_entity.dart
@@ -3,12 +3,13 @@ import 'dart:math';
 import 'package:equatable/equatable.dart';
 
 class TaskEntity extends Equatable {
-  final int id = Random().nextInt(999999);
+  final int id;
   final String title;
   final String subtitle;
   final String state;
 
   TaskEntity({
+    required this.id,
     required this.title,
     required this.subtitle,
     required this.state,
--- test/mock/task_mocks.dart
+++ test/mock/task_mocks.dart
@@ -1,6 +1,7 @@
 import 'package:taskee/domain/entities/entities.dart';
 
 final kTaskEntity = TaskEntity(
+  id: 1,
   title: 'title',
   subtitle: 'subtitle',
   state: 'toDo',
