3a6bad7782d3f54080b419a836625745929caed0
Caio Jesus <caio.nascimento@grupofasitec.com.br>
2021-08-05 08:01:21 -0300

    feat: add RegisterPage translations

--- lib/core/translations/pt_BR/pt_br_translation.dart
+++ lib/core/translations/pt_BR/pt_br_translation.dart
@@ -6,5 +6,7 @@ final Map<String, String> ptBR = {
   'Create your account': 'Crie sua conta',
   'Confirm your password': 'Confirme sua senha',
   'Email text field': 'Campo de email',
-  'Password text field': 'Campo de senha'
+  'Password text field': 'Campo de senha',
+  'Confirm your password text field': 'Campo de confirmar senha',
+  'Sign up': 'Criar conta',
 };
