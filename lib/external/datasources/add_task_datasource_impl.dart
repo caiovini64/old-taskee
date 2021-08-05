1732d1d0eb419e274b15dbdadd304f0d435f7111
Caio Jesus <caio.nascimento@grupofasitec.com.br>
2021-08-05 12:35:37 -0300

    feat: ensure AddTaskDatasource returns a String

---
+++ lib/external/datasources/add_task_datasource_impl.dart
@@ -0,0 +1,24 @@
+import 'package:taskee/infra/datasources/datasources.dart';
+import 'package:taskee/infra/helpers/helpers.dart';
+
+class AddTaskDatasource extends IAddTaskDatasource {
+  final IConnectionClient client;
+  final String url;
+  AddTaskDatasource({
+    required this.client,
+    required this.url,
+  });
+
+  @override
+  Future<String> addTask(String title, String subtitle, String state) async {
+    final response = await client.post(
+      url,
+      body: {
+        "title": title,
+        "subtitle": subtitle,
+        "state": state,
+      },
+    );
+    return response.data;
+  }
+}
