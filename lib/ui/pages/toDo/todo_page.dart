25f226272f906b6cf1a44612c1bcbc8729e43a8b
Caio Jesus <caio.nascimento@grupofasitec.com.br>
2021-08-05 14:47:14 -0300

    feat: create FloatingActionBar widget

--- lib/ui/pages/toDo/todo_page.dart
+++ lib/ui/pages/toDo/todo_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:taskee/ui/helpers/app_colors.dart';
+import 'package:taskee/ui/widgets/floating_action_bar_widget.dart';
 import 'package:taskee/ui/widgets/widgets.dart';
 
 class ToDoPage extends StatelessWidget {
@@ -7,52 +8,59 @@ class ToDoPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        child: Column(
-          children: [
-            SizedBox(height: 100),
-            Expanded(
-              child: SingleChildScrollView(
-                scrollDirection: Axis.vertical,
-                child: Container(
-                  height: MediaQuery.of(context).size.height,
-                  width: MediaQuery.of(context).size.width,
-                  padding: EdgeInsets.all(20),
-                  decoration: BoxDecoration(
-                    color: Theme.of(context).backgroundColor,
-                    borderRadius:
-                        BorderRadius.only(topLeft: Radius.circular(60)),
-                  ),
-                  child: Column(
-                    children: [
-                      SizedBox(height: 35),
-                      Align(
-                        alignment: Alignment.topLeft,
-                        child: Text(
-                          'To do',
-                          style: Theme.of(context).textTheme.headline1,
-                        ),
+      body: Stack(
+        children: [
+          Container(
+            child: Column(
+              children: [
+                SizedBox(height: 100),
+                Expanded(
+                  child: SingleChildScrollView(
+                    scrollDirection: Axis.vertical,
+                    child: Container(
+                      height: MediaQuery.of(context).size.height,
+                      width: MediaQuery.of(context).size.width,
+                      padding: EdgeInsets.all(20),
+                      decoration: BoxDecoration(
+                        color: Theme.of(context).backgroundColor,
+                        borderRadius:
+                            BorderRadius.only(topLeft: Radius.circular(60)),
                       ),
-                      SizedBox(height: 35),
-                      TaskCardWidget(
-                        cardColor: orangeCardColor,
-                        title: 'title',
-                        subtitle: 'subtitle',
-                        icon: IconButton(
-                          icon: Icon(
-                            Icons.arrow_forward_outlined,
-                            color: Colors.white,
+                      child: Column(
+                        children: [
+                          SizedBox(height: 35),
+                          Align(
+                            alignment: Alignment.topLeft,
+                            child: Text(
+                              'To do',
+                              style: Theme.of(context).textTheme.headline1,
+                            ),
                           ),
-                          onPressed: () {},
-                        ),
+                          SizedBox(height: 35),
+                          TaskCardWidget(
+                            cardColor: orangeCardColor,
+                            title: 'title',
+                            subtitle: 'subtitle',
+                            icon: IconButton(
+                              icon: Icon(
+                                Icons.arrow_forward_outlined,
+                                color: Colors.white,
+                              ),
+                              onPressed: () {},
+                            ),
+                          ),
+                        ],
                       ),
-                    ],
+                    ),
                   ),
                 ),
-              ),
+              ],
             ),
-          ],
-        ),
+          ),
+          FloatingActionWidget(
+            onTap: () => print('oi'),
+          ),
+        ],
       ),
     );
   }
