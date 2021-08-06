195760a611b1a46ffe9dae4ec4e9bf11a28ccb2c
Caio Jesus <caio.nascimento@grupofasitec.com.br>
2021-08-06 15:14:08 -0300

    feat: update design

--- lib/ui/pages/toDo/todo_page.dart
+++ lib/ui/pages/toDo/todo_page.dart
@@ -51,6 +51,31 @@ class ToDoPage extends StatelessWidget {
                               onPressed: () {},
                             ),
                           ),
+                          TaskCardWidget(
+                            cardColor: orangeCardColor,
+                            title: 'title',
+                            subtitle:
+                                "subtitle Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, b",
+                            icon: IconButton(
+                              icon: Icon(
+                                Icons.arrow_forward_outlined,
+                                color: Colors.white,
+                              ),
+                              onPressed: () {},
+                            ),
+                          ),
+                          TaskCardWidget(
+                            cardColor: orangeCardColor,
+                            title: 'title',
+                            subtitle: 'subtitle',
+                            icon: IconButton(
+                              icon: Icon(
+                                Icons.arrow_forward_outlined,
+                                color: Colors.white,
+                              ),
+                              onPressed: () {},
+                            ),
+                          ),
                         ],
                       ),
                     ),
--- lib/ui/widgets/task_card_widget.dart
+++ lib/ui/widgets/task_card_widget.dart
@@ -16,40 +16,43 @@ class TaskCardWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      color: cardColor,
-      child: Container(
-        width: MediaQuery.of(context).size.width,
-        padding: EdgeInsets.only(left: 20, right: 20, bottom: 40, top: 20),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Expanded(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    title,
-                    style: Theme.of(context)
-                        .textTheme
-                        .headline4!
-                        .copyWith(color: Colors.white),
-                    maxLines: 1,
-                    overflow: TextOverflow.ellipsis,
-                  ),
-                  SizedBox(height: 10),
-                  Text(
-                    subtitle,
-                    style: Theme.of(context).textTheme.bodyText2,
-                    maxLines: 5,
-                    overflow: TextOverflow.ellipsis,
-                  ),
-                ],
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 10.0),
+      child: Card(
+        color: cardColor,
+        child: Container(
+          width: MediaQuery.of(context).size.width,
+          padding: EdgeInsets.only(left: 20, right: 20, bottom: 30, top: 20),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Expanded(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      title,
+                      style: Theme.of(context)
+                          .textTheme
+                          .headline4!
+                          .copyWith(color: Colors.white),
+                      maxLines: 1,
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                    SizedBox(height: 10),
+                    Text(
+                      subtitle,
+                      style: Theme.of(context).textTheme.bodyText2,
+                      maxLines: 5,
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                  ],
+                ),
               ),
-            ),
-            icon,
-          ],
+              icon,
+            ],
+          ),
         ),
       ),
     );
