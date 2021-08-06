7eb375c8055e232f071f4567e3e879c155870abf
Caio Jesus <caio.nascimento@grupofasitec.com.br>
2021-08-06 10:56:57 -0300

    feat: add form

--- lib/ui/pages/newTask/controller/new_task_controller.dart
+++ lib/ui/pages/newTask/controller/new_task_controller.dart
@@ -1,19 +1,18 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:get/get.dart';
 import 'package:taskee/domain/usecases/usecases.dart';
 
 part 'new_task_state.dart';
 
 class NewTaskController extends Cubit<NewTaskState> {
-  final AddTaskUsecase addTaskUsecase;
-  NewTaskController(this.addTaskUsecase) : super(NewTaskInitial());
+  final AddTaskUsecase _addTaskUsecase;
+  NewTaskController(this._addTaskUsecase) : super(NewTaskInitial());
 
   void addTask(String title, String subtitle, String state) async {
     emit(NewTaskLoading());
-    final result = await addTaskUsecase(title, subtitle, state);
-    result.fold(
-      (failure) => emit(NewTaskError()),
-      (data) => emit(NewTaskDone()),
-    );
+    await _addTaskUsecase(title, subtitle, state);
+    emit(NewTaskDone());
+    Get.back();
   }
 }
