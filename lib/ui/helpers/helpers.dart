65e231b6d4dc5a5cdb3e78d9b524bc9ae00095fd
Caio Jesus <caio.nascimento@grupofasitec.com.br>
2021-08-11 07:39:39 -0300

    feat: add export files

--- lib/ui/helpers/helpers.dart
+++ lib/ui/helpers/helpers.dart
@@ -1,2 +1,1 @@
-export 'theme/app_theme.dart';
-export 'theme/app_colors.dart';
+export 'theme/themes.dart';
--- lib/ui/widgets/widgets.dart
+++ lib/ui/widgets/widgets.dart
@@ -1,2 +1,3 @@
 export 'custom_textfield_widget.dart';
 export 'task_card_widget.dart';
+export 'floating_action_bar_widget.dart';
