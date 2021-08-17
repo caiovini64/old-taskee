a6b993b8a14894596d8120a6dbfc4d01ecfce675
Caio Jesus <caio.nascimento@grupofasitec.com.br>
2021-08-17 07:39:50 -0300

    refactor: create saveUser method to maintaining the single responsibility of login method

--- lib/ui/pages/login/cubit/login_cubit.dart
+++ lib/ui/pages/login/cubit/login_cubit.dart
@@ -1,10 +1,8 @@
 import 'package:bloc/bloc.dart';
-import 'package:dartz/dartz.dart';
 import 'package:get/get.dart';
 import 'package:get_it/get_it.dart';
 import 'package:meta/meta.dart';
-import 'package:taskee/domain/entities/entities.dart';
-import 'package:taskee/domain/helpers/helpers.dart';
+
 import 'package:taskee/domain/usecases/usecases.dart';
 import 'package:taskee/data/models/models.dart';
 import 'package:taskee/ui/pages/toDo/todo_page.dart';
@@ -19,20 +17,24 @@ class LoginCubit extends Cubit<LoginState> {
 
   Future<void> login(String email, String password) async {
     emit(LoginLoading());
-    final Either<Failure, UserEntity> result =
-        await _loginUsecase.login(email.trim(), password);
-    result.fold((failure) {
-      emit(LoginError(failure.message));
-    }, (right) {
-      emit(LoginDone());
+    final result = await _loginUsecase.login(email.trim(), password);
+    result.fold(
+      (failure) => emit(LoginError(failure.message)),
+      (right) {
+        emit(LoginDone());
+        _saveUser(right.email, right.id, right.token);
+        Get.toNamed(ToDoPage.route);
+      },
+    );
+  }
 
-      serviceLocator.registerLazySingleton<UserModel>(() => UserModel(
-            email: right.email,
-            id: right.id,
-            token: right.token,
-          ));
-
-      Get.toNamed(ToDoPage.route);
-    });
+  void _saveUser(String email, String id, String token) {
+    serviceLocator.registerLazySingleton<UserModel>(
+      () => UserModel(
+        email: email,
+        id: id,
+        token: token,
+      ),
+    );
   }
 }
