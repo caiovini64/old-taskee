c0d2b9b9ded0e22acf06c4283d803a7698202c7b
Caio Jesus <caio.nascimento@grupofasitec.com.br>
2021-08-17 07:22:51 -0300

    refactor: remove unused imports

--- lib/ui/pages/register/cubit/register_cubit.dart
+++ lib/ui/pages/register/cubit/register_cubit.dart
@@ -1,9 +1,6 @@
 import 'package:bloc/bloc.dart';
-import 'package:dartz/dartz.dart';
 import 'package:equatable/equatable.dart';
 
-import 'package:taskee/domain/entities/entities.dart';
-import 'package:taskee/domain/helpers/helpers.dart';
 import 'package:taskee/domain/usecases/usecases.dart';
 
 part 'register_state.dart';
@@ -15,12 +12,10 @@ class RegisterCubit extends Cubit<RegisterState> {
 
   Future<void> register(String email, String password) async {
     emit(Loading());
-    final Either<Failure, UserEntity> result =
-        await _registerUsecase.register(email.trim(), password);
-    result.fold((failure) {
-      emit(Error(failure.message));
-    }, (right) {
-      emit(Success());
-    });
+    final result = await _registerUsecase.register(email.trim(), password);
+    result.fold(
+      (failure) => emit(Error(failure.message)),
+      (right) => emit(Success()),
+    );
   }
 }
