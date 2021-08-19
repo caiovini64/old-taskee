b3ea42f115ae01fa216b52bde16a3292697b96e1
Caio Jesus <caio.nascimento@grupofasitec.com.br>
2021-08-19 10:38:25 -0300

    refactor: remove useless code

--- lib/ui/mixins/tasks_manager.dart
+++ lib/ui/mixins/tasks_manager.dart
@@ -4,10 +4,6 @@ import 'package:taskee/ui/helpers/states/task_state.dart';
 mixin TasksManager {
   filterTasks(
       {required List<TaskEntity> taskList, required TaskState taskState}) {
-    return _filterTag(taskList, taskState);
-  }
-
-  _filterTag(List<TaskEntity> taskList, TaskState taskState) {
     return taskList
         .where((element) => element.state.contains(taskState.description))
         .toList();
