cfa0e20257ebabe2a4e39f611f7c980ece91e848
Caio Jesus <caio.nascimento@grupofasitec.com.br>
2021-08-24 10:15:08 -0300

    feat: ensure UpdateTaskUsecase returns a ServerFailure when calls to datasource throws a ServerException

--- lib/data/usecases/update_task_usecase_impl.dart
+++ lib/data/usecases/update_task_usecase_impl.dart
@@ -1,7 +1,9 @@
+import 'package:taskee/data/helpers/exceptions/exceptions.dart';
 import 'package:taskee/domain/datasources/datasources.dart';
 import 'package:taskee/domain/helpers/failures/failure.dart';
 import 'package:taskee/domain/entities/task_entity.dart';
 import 'package:dartz/dartz.dart';
+import 'package:taskee/domain/helpers/failures/failures.dart';
 import 'package:taskee/domain/usecases/update_task_usecase.dart';
 
 class UpdateTaskUsecase implements IUpdateTaskUsecase {
@@ -10,7 +12,11 @@ class UpdateTaskUsecase implements IUpdateTaskUsecase {
 
   @override
   Future<Either<Failure, TaskEntity>> update(TaskEntity task) async {
-    final result = await datasource.updateTask(task);
-    return Right(result);
+    try {
+      final result = await datasource.updateTask(task);
+      return Right(result);
+    } on ServerException catch (e) {
+      return Left(ServerFailure());
+    }
   }
 }
--- test/data/usecase/update_task_usecase_impl_test.dart
+++ test/data/usecase/update_task_usecase_impl_test.dart
@@ -1,9 +1,11 @@
 import 'package:dartz/dartz.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
+import 'package:taskee/data/helpers/helpers.dart';
 import 'package:taskee/data/usecases/usecases.dart';
 import 'package:taskee/domain/datasources/update_task_datasource.dart';
 import 'package:taskee/domain/entities/entities.dart';
+import 'package:taskee/domain/helpers/failures/failures.dart';
 import 'package:taskee/domain/usecases/update_task_usecase.dart';
 
 import '../../mock/task_mocks.dart';
@@ -30,5 +32,12 @@ void main() {
         .thenAnswer((_) async => kTaskModel);
     final result = await usecase.update(kTaskEntity);
     expect(result, Right(kTaskModel));
+  });
+  test(
+      'should returns a ServerFailure when calls to datasource throws a ServerException',
+      () async {
+    when(() => datasource.updateTask(any())).thenThrow(ServerException());
+    final result = await usecase.update(kTaskEntity);
+    expect(result, Left(ServerFailure()));
   });
 }
