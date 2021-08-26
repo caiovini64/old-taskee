bc8d8912b1970cfca5f89fe177a41d7401d114aa
Caio Jesus <caio.nascimento@grupofasitec.com.br>
2021-08-26 13:58:27 -0300

    feat: add divider

--- lib/ui/pages/home/components/child_page.dart
+++ lib/ui/pages/home/components/child_page.dart
@@ -50,6 +50,12 @@ class ChildPage extends StatelessWidget with UIErrorManager, TasksManager {
                               style: Theme.of(context).textTheme.headline1,
                             ),
                           ),
+                          SizedBox(height: 2),
+                          Divider(
+                            color: Colors.black.withOpacity(0.3),
+                            height: 2,
+                            thickness: 1,
+                          ),
                           buildTaskList(context),
                         ],
                       ),
